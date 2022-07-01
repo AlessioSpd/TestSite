e5d63127749c1ef97eae10e14487db196fe3798a
Alessio <alessiospadafora.main@gmail.com>
2022-07-01 15:24:43 +0200

    fix problema animazione

--- resource/js/animatorHandler.js
+++ resource/js/animatorHandler.js
@@ -1,16 +1,80 @@
+var check = false;
+
 function logoUp () {
-    document.getElementById("mainPanel")
-        .style = "animation-name: logoUp; animation-duration: 2s; animation-fill-mode: forwards;";
     
-    document.getElementById("nav")
-        .style = "animation-name: navResize; animation-duration: 2s; animation-fill-mode: forwards;"
+    if(!check){
+        document.getElementById("mainPanel").style = `
+            animation-name: logoUp, fadeIn;
+            animation-duration: 2s, 2s;
+            animation-delay: 0s, .5s;
+            animation-fill-mode: forwards;
+        `;
+        
+        document.getElementById("nav").style = `
+            animation-name: navResize;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+        `;
+        
+        document.getElementById("logo").style = `
+            animation-name: logoResize;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;
+        
+        document.getElementById("rightButton").style = `
+            animation-name: fadeIn;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;    
+        
+        document.getElementById("leftButton").style = `
+            animation-name: fadeIn;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;
+    }
     
-    document.getElementById("logo")
-        .style = "animation-name: logoResize; animation-duration: 2s; animation-fill-mode: forwards; animation-delay: .5;"
-    
-    document.getElementById("rightButton")
-        .style = "animation-name: fadeIn; animation-duration: 2s; animation-fill-mode: forwards; animation-delay: .5;"    
-    
-    document.getElementById("leftButton")
-        .style = "animation-name: fadeIn; animation-duration: 2s; animation-fill-mode: forwards; animation-delay: .5;"    
 }
+
+window.addEventListener('load', (event) => {
+
+    if(check) {
+        document.getElementById("mainPanel").style = `
+            animation-name: logoUp, fadeIn;
+            animation-duration: 2s, 2s;
+            animation-delay: 0s, .5s;
+            animation-fill-mode: forwards;
+        `;
+        
+        document.getElementById("nav").style = `
+            animation-name: navResize;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+        `;
+        
+        document.getElementById("logo").style = `
+            animation-name: logoResize;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;
+        
+        document.getElementById("rightButton").style = `
+            animation-name: fadeIn;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;    
+        
+        document.getElementById("leftButton").style = `
+            animation-name: fadeIn;
+            animation-duration: 2s;
+            animation-fill-mode: forwards;
+            animation-delay: .5;
+        `;
+    }
+});
